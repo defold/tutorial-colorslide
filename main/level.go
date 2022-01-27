373831d3658b87fea90cbfc6c47631e168b28d92
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-01-27 09:55:23 +0100

    Added all changes to reach the end of the tutorial

--- main/level.go
+++ main/level.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "gui"
+  component: "/main/level.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "brickfactory"
   type: "collectionfactory"
