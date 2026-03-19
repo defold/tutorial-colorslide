e998986a2e564b4c8ce6343aa4556c76d998b875
paweljarosz <pawel.jarosz21@gmail.com>
2026-03-19 19:38:05 +0100

    Tutorial Colorslide is done

--- main/level.go
+++ main/level.go
@@ -1,17 +1,10 @@
 components {
   id: "level"
   component: "/main/level.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
+}
+components {
+  id: "gui"
+  component: "/main/level.gui"
 }
 components {
   id: "gui"
@@ -32,17 +25,5 @@ embedded_components {
   id: "brickfactory"
   type: "collectionfactory"
   data: "prototype: \"/main/brick/brick.collection\"\n"
-  "load_dynamically: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
